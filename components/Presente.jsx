842e7545202ef405a3525af548679ff1b3d22b99
naiaragabriela <naigabi31@gmail.com>
2024-10-09 21:28:18 -0300

    Feat: initial commit

---
+++ components/Presente.jsx
@@ -0,0 +1,13 @@
+import styles from '../styles/Presente.module.css';
+
+
+export default function Presente() {
+    return (
+        <div className={styles.presente}>
+            <div className={styles.tampa}></div>
+            <div className={styles.corpo}></div>
+            <div className={styles.laco}></div>
+            <div className={styles.fita}></div>
+        </div>
+    )
+}
