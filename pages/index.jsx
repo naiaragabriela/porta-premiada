d42cd7fd4b40c277da268104729cf1f9c67ebe3c
naiaragabriela <naigabi31@gmail.com>
2024-10-09 23:40:59 -0300

    Feat: Door Component

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,12 @@
 import Presente from "../components/Presente";
+import Porta from "../components/Porta";
 
 export default function Home() {
   return (
-    <>
+    <div style={{display: "flex"}}>
       <Presente />
-    </>
+      <Porta selecionada />
+      <Porta />
+    </div>
   );
 }
