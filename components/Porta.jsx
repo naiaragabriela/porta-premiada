30fa47cd68cabeb9f3151de2e97312268e9d093d
naiaragabriela <naigabi31@gmail.com>
2024-10-10 00:00:29 -0300

    Feat: Door class

--- components/Porta.jsx
+++ components/Porta.jsx
@@ -2,6 +2,7 @@ import styles from '../styles/Porta.module.css';
 
 export default function Porta(props) {
     const selecionada = props.selecionada ? styles.selecionada : ''
+    
     return(
         <div className={styles.area}>
         <div className={`${styles.estrutura} ${selecionada}`}>
