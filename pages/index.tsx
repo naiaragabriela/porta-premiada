026bd13479a60ea87e48c007a6da79314eaa5f73
naiaragabriela <naigabi31@gmail.com>
2024-10-14 20:17:57 -0300

    Feat: Add events

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,12 +4,10 @@ import PortaModel from "../model/porta";
 
 export default function Home() {
   const [p1, setP1] = useState(new PortaModel(1))
- 
 
   return (
-    <div style={{display: "flex"}}>
-      <Porta porta={p1} />
-     
+    <div style={{display: "flex", flexDirection: "column"}}>
+      <Porta value={p1} onChange={novaPorta => setP1(novaPorta)} />
     </div>
   );
 }
