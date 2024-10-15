119f7223189ff15386b00b14cad50d7e099bd65a
naiaragabriela <naigabi31@gmail.com>
2024-10-14 21:09:12 -0300

    Feat: Created a Game

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,19 +4,11 @@ import PortaModel from "../model/porta";
 import { atualizarPortas, criarPortas } from "../functions/portas";
 
 export default function Home() {
-  const [portas, setPortas] = useState(criarPortas(3,2))
-  
-  function renderizarPortas() {
-    return portas.map(porta => {
-      return <Porta key={porta.numero} value={porta} 
-        onChange={novaPorta => setPortas(atualizarPortas(portas, novaPorta))} 
-      />
-    })
-  }
+ 
 
   return (
-    <div style={{display: "flex"}}>
-      {renderizarPortas()}
-    </div>
+    <>
+    <h1>Inicio do Jogo</h1>
+    </>
   );
 }
